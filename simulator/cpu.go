985e9f3b6a87a83ea92e07e1b4565bf6e2f593c5
sinaroydel <3inaroydl@gmail.com>
2024-11-03 11:38:07 +0330

    add cpu struct

--- simulator/cpu.go
+++ simulator/cpu.go
@@ -4,3 +4,10 @@ type Instruction struct {
 	opcode  string
 	operand int
 }
+
+type Cpu struct {
+	PC     int         // program counter
+	AC     int         // Accumulator
+	IR     Instruction // instruction register
+	Halted bool        // cpu halt status
+}
