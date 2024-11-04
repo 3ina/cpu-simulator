3d690cf0717a8ba6552a0a26b2803074dafbd427
sinaroydel <3inaroydl@gmail.com>
2024-11-04 13:11:57 +0330

    add Read method to memory struct

--- simulator/memory.go
+++ simulator/memory.go
@@ -7,3 +7,11 @@ type Memory struct {
 func NewMemory() *Memory {
 	return &Memory{data: make(map[int]Instruction)}
 }
+
+func (mem *Memory) Read(address int) Instruction {
+	if instr, exists := mem.data[address]; exists {
+		return instr
+	}
+	// Return a default NOP (No Operation)
+	return Instruction{"NOP", 0}
+}
