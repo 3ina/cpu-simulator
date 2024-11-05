e251d317571ecae4382e03b378ec0bf825bde9ed
sinaroydel <3inaroydl@gmail.com>
2024-11-05 10:14:00 +0330

    add DecodeAndExecute method to cpu struct

--- simulator/cpu.go
+++ simulator/cpu.go
@@ -1,5 +1,7 @@
 package simulator
 
+import "fmt"
+
 type Instruction struct {
 	opcode  string
 	operand int
@@ -15,4 +17,48 @@ type Cpu struct {
 func (cpu *Cpu) Fetch(mem *Memory) {
 	cpu.IR = mem.Read(cpu.PC)
 	cpu.PC++
+}
+
+func (cpu *Cpu) DecodeAndExecute(mem *Memory) {
+	switch cpu.IR.opcode {
+	case "LDA":
+		cpu.AC = mem.Read(cpu.IR.operand).operand
+	case "STO":
+		mem.Write(cpu.IR.operand, Instruction{"DATA", cpu.AC})
+	case "ADD":
+		cpu.AC += mem.Read(cpu.IR.operand).operand
+	case "SUB":
+		cpu.AC -= mem.Read(cpu.IR.operand).operand
+	case "MPY":
+		cpu.AC *= mem.Read(cpu.IR.operand).operand
+	case "BRU":
+		cpu.PC = cpu.IR.operand
+	case "BRM":
+		if cpu.AC < 0 {
+			cpu.PC = cpu.IR.operand
+		}
+	case "WWD":
+		value := mem.Read(cpu.IR.operand).operand
+		fmt.Println("OUTPUT:", value)
+	case "RWD":
+		var input int
+		fmt.Print("Enter input value: ")
+		_, err := fmt.Scan(&input)
+
+		if err != nil {
+			fmt.Println("Invalid input, Please enter an integer.")
+			cpu.Halted = true
+			return
+		}
+
+		mem.Write(cpu.IR.operand, Instruction{"DATA", input})
+	case "HLT":
+		cpu.Halted = true
+	case "NOP":
+		// do nothing
+	default:
+		fmt.Println("Unknown instruction: ", cpu.IR.opcode)
+		cpu.Halted = true
+
+	}
 }
